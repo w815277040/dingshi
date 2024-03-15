f49c30baf32f1ca637d690a7e33e8fb41ae5cc13
w815277040 <58384358+w815277040@users.noreply.github.com>
2024-03-15 23:00:33 +0800

    Create dingshi.py
    定时闹钟

---
+++ dingshi.py
@@ -0,0 +1,18 @@
+import time
+import datetime
+
+def set_alarm(alarm_time):
+    while True:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        if current_time == alarm_time:
+            print("Time's up! Wake up!")
+            break
+        else:
+            print("Current time is:", current_time)
+            time.sleep(1)
+
+# 设置闹钟时间，格式为"时:分:秒"，例如 "07:00:00"
+alarm_time = "07:00:00"
+
+# 启动定时器
+set_alarm(alarm_time)
